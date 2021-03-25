91b355f7ffd207a29b36a7bea5972a43facafc3b
reydominictome <70247995+reydominictome@users.noreply.github.com>
2021-03-25 14:35:35 +0800

    NABUANG NA
    HAHAHHAAA

--- metro_events/settings.py
+++ metro_events/settings.py
@@ -144,7 +144,8 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = "/media/"
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+   BASE_DIR / 'static'
 ]
